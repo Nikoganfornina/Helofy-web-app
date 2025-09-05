52b653128c2a03b194593a20348fd2ea53436513
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2025-09-05 14:10:34 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { RegisterPage } from "./pages/RegisterPage";
+import { RegisterPage } from "./pages/registerPage";
 
 const App = () => {
   return (
